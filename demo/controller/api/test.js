86186babd8838065665c175efaecc92e5bbc2e14
wang <1146040444@qq.com>
2025-12-10 14:27:26 +0800

    --

--- demo/controller/api/test.js
+++ demo/controller/api/test.js
@@ -11,6 +11,14 @@ class Test {
     ])
   }
 
+  async post(ctx) {
+    ctx.ok(ctx.body)
+  }
+
+  async list(ctx) {
+    ctx.ok(ctx.query)
+  }
+
   __mid() {
     return [
       {
@@ -19,7 +27,44 @@ class Test {
           await next(ctx)
           console.log(`test ${ctx.method} end`)
         }
-      }
+      },
+
+      {
+        middleware: async (ctx, next) => {
+          console.log('use for get list method')
+          await next(ctx)
+          console.log('end for get list method')
+        },
+
+        handler: [
+          'get', 'list'
+        ]
+      },
+
+      {
+        middleware: async (ctx, next) => {
+          console.log('use for get method', (new Date).toLocaleString())
+          await next(ctx)
+          console.log('end for get method', (new Date).toLocaleString())
+        },
+
+        handler: [
+          'get'
+        ]
+      },
+
+      {
+        middleware: async (ctx, next) => {
+          console.log(' -- use for post method', (new Date).toLocaleString())
+          ctx.body.tag = Math.random()
+          await next(ctx)
+          console.log(' -- end for post method', (new Date).toLocaleString())
+        },
+
+        handler: [
+          'post'
+        ]
+      },
     ]
   }
 }
